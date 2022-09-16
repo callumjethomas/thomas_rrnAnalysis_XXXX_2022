0fc523427123a47124a1e826acb9697c81555385
Callum Thomas <49317461+callumjethomas@users.noreply.github.com>
2022-09-16 15:35:19 +0100

    Fix incorrect curl output directory.

--- code/install_mothur.sh
+++ code/install_mothur.sh
@@ -4,6 +4,6 @@
 # inputs: none
 # outputs: Mothur v.1.48.0 executable files in code/mothur.
 
-curl -kL https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.win.zip -o code/mothur/Mothur.win.zip
-unzip -n -d code/mothur code/mothur/Mothur.win.zip
-rm code/mothur/Mothur.win.zip
+curl -kL https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.win.zip -o code/Mothur.win.zip
+unzip -n -d code/mothur code/Mothur.win.zip
+rm code/Mothur.win.zip
