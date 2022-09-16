bc765fb65e36d6916d3acd352d903beab7c30841
Callum Thomas <49317461+callumjethomas@users.noreply.github.com>
2022-09-16 15:36:58 +0100

    Fix incorrect unzip directory.

--- code/install_mothur.sh
+++ code/install_mothur.sh
@@ -5,5 +5,5 @@
 # outputs: Mothur v.1.48.0 executable files in code/mothur.
 
 curl -kL https://github.com/mothur/mothur/releases/download/v1.48.0/Mothur.win.zip -o code/Mothur.win.zip
-unzip -n -d code/mothur code/Mothur.win.zip
+unzip -n -d code code/Mothur.win.zip
 rm code/Mothur.win.zip
