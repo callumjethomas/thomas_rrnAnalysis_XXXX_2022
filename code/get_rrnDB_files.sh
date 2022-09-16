5bebe7251a0e7f9974cd3d17952c3154285e3934
Callum Thomas <callumthomas@outlook.com>
2022-09-16 15:13:24 +0100

    Automate downloading rrnDB and SILVA SEED files.

---
+++ code/get_rrnDB_files.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# author: Callum Thomas
+# inputs: Name of the file to be extracted from the archive (without the path).
+# outputs: Appropriate rrnDB version 5.8 file, into data/raw.
+
+archive=$1
+
+echo "$archive"
+curl -kL https://rrndb.umms.med.umich.edu/static/download/"$archive".zip -o data/raw/"$archive".zip
+unzip -n -d data/raw data/raw/"$archive".zip
+rm data/raw/"$archive".zip
---
+++ code/get_silva_seed.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# author: Callum Thomas
+# inputs: none
+# outputs: places SILVA SEED ref. alignment into data/references/silva_seed
+
+# Automate retreiving SILVA SEED reference sequence to help aligning our
+# sequence data. Get version 138.1. Because .tgz file contains a README file,
+# we extract to subdirectory 'silva_seed' to not overwrite existing README.
+
+curl -kL https://mothur.s3.us-east-2.amazonaws.com/wiki/silva.seed_v138_1.tgz -o data/references/silva.seed_v138_1.tgz
+mkdir data/references/silva_seed
+tar xvzf data/references/silva.seed_v138_1.tgz -C data/references/silva_seed
+rm data/references/silva.seed_v138_1.tgz
