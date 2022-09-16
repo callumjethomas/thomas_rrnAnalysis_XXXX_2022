c9f8a1fc70ad3e66b60ad12b20ad04c631bae548
Callum Thomas <49317461+callumjethomas@users.noreply.github.com>
2022-09-16 16:02:20 +0100

    Update input/outputs, add note re: flip option.

--- code/align_sequences.sh
+++ code/align_sequences.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # author: Callum Thomas
-# inputs: none
-# outputs: Mothur alignment and logfile.
+# inputs: data/raw/rrnDB-5.8_16S_rRNA.fasta
+#         data/references/silva_seed/silva.seed_v138_1.align
+# outputs: data/raw/rrnDB-5.8_16S_rRNA.align and logfile.
+
+# NB: We need to include flip=T to ensure all sequences are pointed in the same direction.
 
 code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.8_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
